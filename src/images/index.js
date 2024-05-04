66a15ab7113137f6bc10c5b807a01f2308530166
love olubummo <olubummolove3@gmail.com>
2024-05-04 09:15:52 +0100

    feature: created a named export for images and placed structure for web page

---
+++ src/images/index.js
@@ -0,0 +1,36 @@
+import ali from './avatar-ali.png';
+import anisha from './avatar-anisha.png';
+import richard from './avatar-richard.png';
+import shanai from './avatar-shanai.png';
+import simplifyDesktop from './bg-simplify-section-desktop.svg';
+import simplifyMobile from './bg-simplify-section-mobile.svg';
+import tablet from './bg-tablet-pattern.svg';
+import iconClose from './icon-close.svg';
+import facebook from './icon-facebook.svg';
+import hamburger from './icon-hamburger.svg';
+import instagram from './icon-instagram.svg';
+import pinterest from './icon-pinterest.svg';
+import twitter from './icon-twitter.svg';
+import youtube from './icon-youtube.svg';
+import illustration from './illustration-intro.svg';
+import logo from './logo.svg';
+
+
+export const pics = {
+    ali,
+    anisha,
+    richard,
+    shanai,
+    simplifyDesktop,
+    simplifyMobile,
+    tablet,
+    iconClose,
+    facebook,
+    hamburger,
+    instagram,
+    pinterest,
+    twitter,
+    youtube,
+    illustration,
+    logo
+}
